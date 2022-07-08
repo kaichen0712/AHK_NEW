b2799e7dbbe654f695110d595703a432d55af31a
kaichen0712 <oreo88555@gmail.com>
2022-07-08 10:14:26 +0800

    first commit

--- AHK可用版本/Adventure/001.bat
+++ AHK可用版本/Adventure/001.bat
@@ -2,7 +2,7 @@
 echo *** Loading AutoSub, please wait ...
 set INPUT=%1
 set REAL_OUTPUT=%2
-cd C:\Users\user\Desktop\GIT2\testspeechweifang
+cd C:\Users\user\Desktop\GIThub AHK_NEW\testspeechweifang
 main.py -f %1
 cd..
 rem Open the output
